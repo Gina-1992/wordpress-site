e2733c3f4a0cf286303c05043e4f8f7e7eacbe4f
Gina-1992 <georgina.henderson92@gmail.com>
2019-01-03 07:03:42 +0000

    url fix

---
+++ wp-content/themes/BristolUniversity/index.php
@@ -0,0 +1,15 @@
+<?php
+
+/*function greet ($name, $color) {
+	echo "Hello, my name is $name and my favourite color is $color</p>";
+}
+
+greet('John' ,'blue');
+greet('Jane', 'pink');
+
+?><h1><?php bloginfo('description'); ?></h1>
+*/
+
+$myName = "Gina";
+
+?>
